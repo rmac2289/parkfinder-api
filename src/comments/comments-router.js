afa2d0bf9b7e2e4f0586b15b5d520dc0f7afadb7
Ross MacDonald <rmac2289@gmail.com>
2020-07-09 03:11:56 -0700

    fixed heroku error

--- src/comments/comments-router.js
+++ src/comments/comments-router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const CommentsService = require('./Comments-service')
+const CommentsService = require('./comments-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 
 const CommentsRouter = express.Router()
