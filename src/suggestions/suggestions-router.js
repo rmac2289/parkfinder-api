a1c6ee922aaf4786ca6cce778a6f31e3d8ae7daf
Ross MacDonald <rmac2289@gmail.com>
2020-07-10 04:51:16 -0700

    fixed bug

---
+++ src/suggestions/suggestions-router.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const SuggestionsService = require('./suggestions-service')
+const { requireAuth } = require('../middleware/jwt-auth')
+
+const SuggestionsRouter = express.Router()
+const jsonBodyParser = express.json()
+
+// routes for suggestions //
+
+SuggestionsRouter
+  .route('/')
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const { location, park_name, description } = req.body 
+    const newSuggestions =  { location, park_name, description } 
+
+    for (const [key, value] of Object.entries(newSuggestions))
+      if (value == null)
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        })
+
+    SuggestionsService.insertSuggestions(
+      req.app.get('db'),
+      newSuggestions
+    )
+      .then(suggestions => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${suggestions.id}`))
+          .json(SuggestionsService.serializeSuggestions(suggestions))
+      })
+      .catch(error)
+  })
+  
+
+module.exports = SuggestionsRouter
---
+++ src/suggestions/suggestions-service.js
@@ -0,0 +1,44 @@
+const xss = require('xss')
+
+const SuggestionsService = {
+
+  getAllP(db) {
+    return db
+      .from('parkfinder_suggestions AS sug')
+      .select(
+        'sug.id',
+        'sug.park_name',
+        'sug.location',
+        'sug.description',
+      )
+      .groupBy('sug.id')
+  },
+
+  getById(db, id) {
+    return SuggestionsService.getAllSuggestions(db)
+      .where('sug.id', id)
+      .first()
+  },
+  insertSuggestions(db, newSuggestions) {
+    return db
+      .insert(newSuggestions)
+      .into('parkfinder_suggestions')
+      .returning('*')
+      .then(([suggestions]) => suggestions)
+      .then(suggestions =>
+        SuggestionsService.getById(db, suggestions.id)
+      )
+  },
+
+  serializeSuggestions(suggestions) {
+    const { user } = suggestions
+    return {
+      id: suggestions.id,
+      park_name: xss(suggestions.park_name),
+      sugments: xss(suggestions.location),
+      description: xss(suggestions.description)
+    }
+  }
+}
+
+module.exports = SuggestionsService
